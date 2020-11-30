807e6c2bd699a208f638d41b0b53407e4f609d2d
“帮帮礼” <m13220298345@163.com>
2020-11-30 17:21:09 +0800

    提交

---
+++ app/src/main/java/com/huoli/bbl/focusdemo/ListActivity.java
@@ -0,0 +1,19 @@
+package com.huoli.bbl.focusdemo;
+
+import android.app.Activity;
+
+/**
+ * ===============================
+ * 千万不要出现 B U G ，出现就 G G
+ * THE BEST CODE IS NO CODE
+ * ===============================
+ *
+ * @author: yfl
+ * @date: 2020-11-30
+ * @description: 就是一个普通类
+ * @lastUpdateTime 2020-11-30
+ * #更新内容
+ * ===============================
+ **/
+class ListActivity extends Activity {
+}
